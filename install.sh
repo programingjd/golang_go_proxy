a0f9522b534130707fad06061107b92fd91fa0de
Jerome David <programingjd@gmail.com>
2016-04-30 02:45:35 +0200

    -

--- install.sh
+++ install.sh
@@ -7,28 +7,32 @@ pushd $dir
 GO_VERSION=1.6.2
 
 case "$OSTYPE" in
-  linux*)  DISTRIBUTION_ARCHIVE=go${GO_VERSION}.linux-amd64.tar.gz ;;
-  darwin*) DISTRIBUTION_ARCHIVE=go${GO_VERSION}.darwin-amd64.tar.gz ;;
+  linux*)  DISTRIBUTION_ARCHIVE=go${GO_VERSION}.linux-amd64.tar ;;
+  darwin*) DISTRIBUTION_ARCHIVE=go${GO_VERSION}.darwin-amd64.tar ;;
   *) echo Unsupported platform && exit 1 ;;
 esac
 
-URL=https://storage.googleapis.com/golang/$DISTRIBUTION_ARCHIVE
+URL=https://storage.googleapis.com/golang/$DISTRIBUTION_ARCHIVE.gz
 
-if [ -f $DISTRIBUTION_ARCHIVE ];
+if [ ! -f $DISTRIBUTION_ARCHIVE ];
 then
   echo Downloading the go distribution archive.
-  curl --no-check-certificate $URL
+  curl -k -O $URL
+  gunzip go${GO_VERSION}.*.tar.gz
 fi
 
 echo Extracting the go distribution archive.
-gunzip go${GO_VERSION}.*.tar.gz
 tar xf go${GO_VERSION}.*.tar
 
 echo Compiling the go helper.
-GOROOT=$dir/go
-GOPATH=$dir
-GOBIN=$dir/bin
+export GOROOT=$dir/go
+export GOPATH=$dir
+export GOBIN=$dir/bin
 $GOROOT/bin/go install src/go.go
+
+export GOROOT=
+export GOPATH=
+export GOBIN=
 
 echo Updating the go distribution.
 if [ -f go/bin/_go ]; then rm -f go/bin/_go; fi;
